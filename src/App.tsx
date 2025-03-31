66f40caabd56d7e9b493df38a8ebab77628aa6f5
Amit Vikram <amitv@aereo.io>
2025-03-31 17:09:35 +0530

    Home Page designed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Header from "./components/Header";
-import About from "./components/About";
 // import './App.css'
 
 import './sass/main.scss'
@@ -9,7 +8,6 @@ function App() {
   return (
     <>
     <Header/>
-    <About/>
     </>
   )
 }
