a88786a4858ebaccd7b04a232f265d53ab5b81ca
Amit Vikram <amitv@aereo.io>
2025-04-02 16:43:15 +0530

    Added About Page and some images

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <span>Hola </span>
+            <span>Explorers!!! </span>
+            <span>Welcome </span>
+            <span>to </span>
+            <span>my </span>
+            <span>Portfolio </span>
+            <span>Website</span>
+        </div>
+    )
+}
